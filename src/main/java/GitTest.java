965729e06e8a7de9ceb7a772342f55b0bb0e1e06
itjacksonhu <hjhjackson@126.com>
2023-03-13 11:44:15 +0800

    push commit

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -6,6 +6,7 @@ public class GitTest {
         System.out.println("hello git4");
         System.out.println("master test！");
         System.out.println("hot -fix test!");
+        System.out.println("push test!");
     }
 
 }
