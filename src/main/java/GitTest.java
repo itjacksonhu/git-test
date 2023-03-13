4b6989ffa36c76295af2af2f6b38a8acbe4f3b10
itjacksonhu <127638146+itjacksonhu@users.noreply.github.com>
2023-03-13 11:54:00 +0800

    Update GitTest.java

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -7,6 +7,8 @@ public class GitTest {
         System.out.println("master test！");
         System.out.println("hot -fix test!");
         System.out.println("push test!");
-    }
+        System.out.println("pull test!");
+    
+    }        
 
 }
